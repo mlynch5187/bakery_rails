56abd314ee9b20e2a7e78f1c6f424c15455b2889
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-05 08:54:20 -0600

    Add type to ingredients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410134402) do
+ActiveRecord::Schema.define(version: 20210505145353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190410134402) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+    t.string "type"
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
