dc27780b4d018e2cb7c28914c0d68061fc633b9f
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-05 09:47:12 -0600

    Pie Ingredients can now be created

--- app/controllers/pies/ingredients_controller.rb
+++ app/controllers/pies/ingredients_controller.rb
@@ -8,4 +8,17 @@ class Pies::IngredientsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    ingredient = Ingredient.create(ingredient_params)
+    if ingredient.save
+      redirect_to "/pies/ingredients"
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.permit(:name, :kind)
+  end
 end
