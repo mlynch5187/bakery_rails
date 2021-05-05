b4071a33f4bbc6ae775929d016ecb42f4ed50022
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-05 09:35:59 -0600

    Write pies ingredients show page

---
+++ spec/features/pies/ingredients/new_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "Pie Ingredients New Page", type: :feature do
+  before(:each) do
+    visit "/pies/ingredients"
+  end
+
+  it "It allows me to create a new ingredient" do
+    expect(page).to have_link("New Ingredient")
+    click_link("New Ingredient")
+    expect(current_path).to eq("/pies/ingredients/new")
+
+    name = "Butter"
+    kind = "Oil"
+
+    fill_in :name, with: name
+    fill_in :kind, with: kind
+
+    click_link "Create New Ingredient"
+
+    expect(current_path).to eq("/pies/ingredients")
+    expect(page).to have_content("Butter")
+  end
+end
