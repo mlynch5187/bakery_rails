268ccef807414fce7d42e17161caa81c53355df2
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-05 08:53:21 -0600

    Write pie ingredient show spec

---
+++ spec/features/pies/ingredients/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "Pie Ingredients Show Page", type: :feature do
+  before(:each) do
+    @salt = Ingredient.create(name: "Salt", type: "Condiment")
+    @pepper = Ingredient.create(name: "Pepper", type: "Condiment")
+    @pepper = Ingredient.create(name: "Butter", type: "Oil")
+    visit "/pies/ingredients"
+  end
+
+  it "It allows me to click on an ingredient" do
+    expect(page).to have_link("Salt")
+    click_link("Salt")
+    expect(current_path).to_eq("/pies/ingredients/1")
+    expect(page).to have_content("This ingredient is called Salt")
+    expect(page).to have_content("This ingredient is a Condiment")
+  end
+end
