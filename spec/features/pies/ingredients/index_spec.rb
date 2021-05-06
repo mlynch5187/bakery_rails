13c4502380ca1e33ae37e23aec2fca5982411885
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-06 13:38:29 -0600

    Update dlete route in index view

--- spec/features/pies/ingredients/index_spec.rb
+++ spec/features/pies/ingredients/index_spec.rb
@@ -8,23 +8,20 @@ RSpec.describe "Pie Ingredients Index", type: :feature do
   end
 
   it "It shows a list of ingredients" do
-    save_and_open_page
     expect(page).to have_content("Salt")
     expect(page).to have_content("Condiment")
     expect(page).to have_content("Pepper")
     expect(page).to have_content("Condiment")
   end
 
-  # it "I can delete an ingredient" do
-  #   require "pry"; binding.pry
-  #   save_and_open_page
-  #   expect(page).to have_link("Delete Salt")
-  #   expect(page).to have_link("Delete Pepper")
-  #
-  #   click_link("Delete Salt")
-  #
-  #   expect(current_path).to eq("/pies/ingredients")
-  #   expect(page).to not_have_content("Salt")
-  #   expect(page).to have_content("Pepper")
-  # end
+  it "I can delete an ingredient" do
+    expect(page).to have_link("Delete Salt")
+    expect(page).to have_link("Delete Pepper")
+
+    click_link("Delete Salt")
+
+    expect(current_path).to eq("/pies/ingredients")
+    expect(page).to not_have_content("Salt")
+    expect(page).to have_content("Pepper")
+  end
 end
