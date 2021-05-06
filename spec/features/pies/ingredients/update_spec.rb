6dfd067091a822226d0aff57a30be5774132ef2c
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-06 13:53:29 -0600

    Write ingredient update spec

---
+++ spec/features/pies/ingredients/update_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Pie Ingredients New Page", type: :feature do
+  before(:each) do
+    @salt = Ingredient.create(name: "Salt", kind: "Condiment")
+    @pepper = Ingredient.create(name: "Pepper", kind: "Condiment")
+    visit "/pies/ingredients"
+  end
+
+  it "It allows me to create a new ingredient" do
+    expect(page).to have_link("Update Salt")
+    click_link("Update Salt")
+    expect(current_path).to eq("/pies/ingredients/#{@salt.id}")
+
+    name = "Kosher Salt"
+    kind = "Condiment"
+
+    fill_in :name, with: name
+    fill_in :kind, with: kind
+
+    click_on "Update Ingredient"
+
+    expect(current_path).to eq("/pies/ingredients")
+    expect(page).to have_content("Kosher Salt")
+  end
+end
