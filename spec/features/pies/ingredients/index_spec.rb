7bcaba26987d57dc113d10ea670ee404474ba3c0
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-06 13:30:42 -0600

    Update index spec to look for kind

--- spec/features/pies/ingredients/index_spec.rb
+++ spec/features/pies/ingredients/index_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Ingredients index", type: :feature do
+RSpec.describe "Pie Ingredients Index", type: :feature do
   before(:each) do
-    @salt = Ingredient.create(name: "Salt")
-    @pepper = Ingredient.create(name: "Pepper")
-
+    @salt = Ingredient.create(name: "Salt", kind: "Condiment")
+    @pepper = Ingredient.create(name: "Pepper", kind: "Condiment")
     visit "/pies/ingredients"
   end
 
   it "It shows a list of ingredients" do
     expect(page).to have_content("Salt")
+    expect(page).to have_content("Condiment")
     expect(page).to have_content("Pepper")
+    expect(page).to have_content("Condiment")
   end
+
+  # it "I can delete an ingredient" do
+  #   require "pry"; binding.pry
+  #   save_and_open_page
+  #   expect(page).to have_link("Delete Salt")
+  #   expect(page).to have_link("Delete Pepper")
+  #
+  #   click_link("Delete Salt")
+  #
+  #   expect(current_path).to eq("/pies/ingredients")
+  #   expect(page).to not_have_content("Salt")
+  #   expect(page).to have_content("Pepper")
+  # end
 end
