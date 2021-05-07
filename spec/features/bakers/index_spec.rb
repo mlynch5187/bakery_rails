1092975dd820d0c757440abba6e76e388937251b
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-07 12:24:32 -0600

    Bolster bakers index spec

---
+++ spec/features/bakers/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Bakers Index", type: :feature do
+  before(:each) do
+    @chef_john = Baker.create(name: "Chef John", job: "Head Chef")
+    @chef_alex = Baker.create(name: "Chef Alex", job: "Sous Chef")
+    visit "/bakers"
+
+  it "It shows a list of baker names as links to show pages" do
+    expect(page).to have_link("Chef John")
+    expect(page).to have_link("Chef Alex")
+
+    click_link("Chef John")
+    expect(current_path).to eq("/bakers/#{@chef_john.id}")
+
+    expect(page).to have_link("Return to Bakers Index")
+    click_link("Return to Bakers Index")
+    expect(current_path).to eq("/bakers")
+  end
+end
