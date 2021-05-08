832eae4d78f9e529831357b2fa03f5439a5cc6b1
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-08 11:50:22 -0600

    Write baker index delete spec

--- spec/features/bakers/index_spec.rb
+++ spec/features/bakers/index_spec.rb
@@ -18,4 +18,15 @@ RSpec.describe "Bakers Index", type: :feature do
     click_link("Return to Bakers Index")
     expect(current_path).to eq("/bakers")
   end
+
+  it "I can delete a baker" do
+    expect(page).to have_link("Delete Chef John")
+    expect(page).to have_link("Delete Chef Alex")
+
+    click_link("Delete Chef John")
+
+    expect(current_path).to eq("/bakers")
+    expect(page).to_not have_content("Chef John")
+    expect(page).to have_content("Chef Alex")
+  end
 end
