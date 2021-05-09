61c33a726778dc97a9a77e0d05ab7fd8f03fa9e5
Michael Alex Lynch <mlynch5187@gmail.com>
2021-05-09 10:50:26 -0600

    Write 3rd baker show spec

--- spec/features/bakers/new_spec.rb
+++ spec/features/bakers/new_spec.rb
@@ -36,4 +36,18 @@ RSpec.describe "Bakers New Page", type: :feature do
     expect(current_path).to eq("/bakers/new")
     expect(page).to have_content("Please fill out all fields and give the baker a unique name")
   end
+
+  it "Only allows me to create a baker with a unique name" do
+    @chef_sarah = Baker.create(name: "Chef Sarah", job: "Sous Chef", age: "27")
+    click_link("New Baker")
+
+    fill_in :name, with: "Chef Sarah"
+    fill_in :job, with: "Sous Chef"
+    fill_in :age, with: "27"
+
+    click_on "Create New Baker"
+
+    expect(current_path).to eq("/bakers/new")
+    expect(page).to have_content("Please fill out all fields and give the ingredient a unique name")
+  end
 end
